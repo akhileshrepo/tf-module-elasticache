c6ef5752fed10e63581ab9450395b1f8782f97a1
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-23 13:01:27 +0530

    updated code

--- locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  name_prefix = "${var.env}-${var.elasticache_type}-elasticache"
-  tags        = merge(var.tags, { tf-module-name = "elasticache" }, { env = var.env })
-}
--- vars.tf
+++ vars.tf
@@ -1,13 +1,2 @@
-variable "env" {}
-variable "tags" {}
-variable "subnet_ids" {}
-variable "vpc_id" {}
-variable "sg_ingress_cidr" {}
-variable "elasticache_type" {}
-variable "family" {}
-variable "port" {}
-variable "engine" {}
-variable "node_type" {}
-variable "num_cache_nodes" {}
-variable "engine_version" {}
+
 
